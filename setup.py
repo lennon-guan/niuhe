44e95d7f6f8304ac9a4205192a6691359741cf8d
关明 <lennonguan@qq.com>
2014-11-10 10:55:25 +0800

    change requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='A simple webapi framework',
     author='guan ming',
     license='MIT',
-    install_requires=['Flask>=0.10', 'SQLAlchemy>=0.8'],
+    install_requires=['Flask>=0.10', 'Mako>=0.8']
     packages=find_packages(),
     include_package_data=True,
     platforms='any',
