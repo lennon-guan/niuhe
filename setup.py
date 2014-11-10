83d9a9e7df662fd572ad50c2995e7038c1d89880
关明 <lennonguan@qq.com>
2014-11-10 10:07:03 +0800

    fix

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='niuhe',
     version='0.1',
+    url='https://github.com/lennon-guan/niuhe',
     description='A simple webapi framework',
     author='guan ming',
     license='MIT',
+    install_requires=['Flask>=0.10', 'SQLAlchemy>=0.8'],
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
 )
